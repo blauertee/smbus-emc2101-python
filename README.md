e3748be4986443c7177567685790522d7b726f68
Jakob Heinrich <pn@jakobheinrich.de>
2023-12-14 14:05:47 +0100

    Init

---
+++ README.md
@@ -0,0 +1,3 @@
+This is just a starting point to write to the EMC2101 and read from from a unix pc via SMBus.
+
+The register adresses have been copied from [this file](https://github.com/adafruit/Adafruit_CircuitPython_EMC2101/blob/main/adafruit_emc2101/emc2101_regs.py)
---
+++ emc2101_conf.py
@@ -0,0 +1,123 @@
+import smbus
+import time
+
+class Emc2101(object):
+    MFG_ID_SMSC = 0x5D
+    PART_ID_EMC2101 = 0x16
+    PART_ID_EMC2101R = 0x28
+
+    I2C_ADDR = 0x4C
+
+    MAX_LUT_SPEED = 0x3F  # 6-bit value
+    MAX_LUT_TEMP = 0x7F  # 7-bit
+
+    # Bits in device status register for masks etc.
+    STATUS_BUSY = 0x80
+    STATUS_INTHIGH = 0x40
+    STATUS_EEPROM = 0x20
+    STATUS_EXTHIGH = 0x10
+    STATUS_EXTLOW = 0x08
+    STATUS_FAULT = 0x04
+    STATUS_TCRIT = 0x02
+    STATUS_TACH = 0x01
+
+    STATUS_ALERT = (
+        STATUS_TACH
+        | STATUS_TCRIT
+        | STATUS_FAULT
+        | STATUS_EXTLOW
+        | STATUS_EXTHIGH
+        | STATUS_INTHIGH
+    )
+
+    # Bits in device configuration register for masks etc.
+    CONFIG_MASK = 0x80
+    CONFIG_STANDBY = 0x40
+    CONFIG_FAN_STANDBY = 0x20
+    CONFIG_DAC = 0x10
+    CONFIG_DIS_TO = 0x08
+    CONFIG_ALT_TACH = 0x04
+    CONFIG_TCRIT_OVR = 0x02
+    CONFIG_QUEUE = 0x01
+
+    # Values of external temp register for fault conditions.
+    TEMP_FAULT_OPENCIRCUIT = 0x3F8
+    TEMP_FAULT_SHORT = 0x3FF
+
+    # See datasheet section 6.14:
+    FAN_RPM_DIVISOR = 5400000
+
+    #
+    # EMC2101 Register Addresses
+    #
+    INTERNAL_TEMP = 0x00  # Readonly
+    EXTERNAL_TEMP_MSB = 0x01  # Readonly, Read MSB first
+    EXTERNAL_TEMP_LSB = 0x10  # Readonly
+    REG_STATUS = 0x02  # Readonly
+    REG_CONFIG = 0x03  # Also at 0x09
+    CONVERT_RATE = 0x04  # Also at 0x0A
+    INT_TEMP_HI_LIM = 0x05  # Also at 0x0B
+    TEMP_FORCE = 0x0C
+    ONESHOT = 0x0F  # Effectively Writeonly
+    SCRATCH_1 = 0x11
+    SCRATCH_2 = 0x12
+    EXT_TEMP_LO_LIM_LSB = 0x14
+    EXT_TEMP_LO_LIM_MSB = 0x08  # Also at 0x0E
+    EXT_TEMP_HI_LIM_LSB = 0x13
+    EXT_TEMP_HI_LIM_MSB = 0x07  # Also at 0x0D
+    ALERT_MASK = 0x16
+    EXT_IDEALITY = 0x17
+    EXT_BETACOMP = 0x18
+    TCRIT_TEMP = 0x19
+    TCRIT_HYST = 0x21
+    TACH_LSB = 0x46  # Readonly, Read MSB first
+    TACH_MSB = 0x47  # Readonly
+    TACH_LIMIT_LSB = 0x48
+    TACH_LIMIT_MSB = 0x49
+    FAN_CONFIG = 0x4A
+    FAN_SPINUP = 0x4B
+    REG_FAN_SETTING = 0x4C
+    PWM_FREQ = 0x4D
+    PWM_FREQ_DIV = 0x4E
+    FAN_TEMP_HYST = 0x4F
+    AVG_FILTER = 0xBF
+
+    REG_PARTID = 0xFD  # Readonly, 0x16 (or 0x28 for -R part)
+    REG_MFGID = 0xFE  # Readonly, SMSC is 0x5D
+    REG_REV = 0xFF  # Readonly, e.g. 0x01
+
+    LUT_HYSTERESIS = 0x4F
+    LUT_BASE = 0x50
+
+    I2CADDRESS = 0x4C
+
+    def __init__(self, i2cbus):
+        self.BUS = smbus.SMBus(i2cbus)
+
+    def _read(self, register_address):
+        return self.BUS.read_byte_data(self.I2CADDRESS, register_address)
+    
+    def _write(self, register_address, data_byte):
+        return self.BUS.write_byte_data(i2CADDRESS, register_address, data_byte)
+
+    def close(self):
+        self.BUS.close()
+    
+    @property
+    def internalTemp(self):
+        return int(self._read(Emc2101.INTERNAL_TEMP))
+
+    @property
+    def dacMode(self):
+        return bool(self._read(Emc2101.REG_CONFIG) & Emc2101.CONFIG_DAC)
+
+
+
+
+# Define the I2C bus number
+bus_number = 10
+
+emc = Emc2101(bus_number)
+print(emc.internalTemp)
+print(emc.dacMode)
+emc.close()
